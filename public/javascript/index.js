3f8ed6e4c6e0007daf89cc860ebc958095b7d962
Harshvardhan Raj <thehvr1@gmail.com>
2025-10-02 01:06:37 +0530

    Update index.js

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,5 +1,3 @@
-//https://pixabay.com/api/?key=52522940-9783397e1c576a849f4672259&q=yellow+flowers&image_type=photo
-
 const query = "<%= q %>";
 if (query) {
     const logo = document.querySelector("#hoogleLogo");
@@ -9,4 +7,5 @@ if (query) {
 const topBtn = document.querySelector(".topBtn");
 topBtn.addEventListener("click", () => {
     window.scrollTo( {top : 0, behavior : "smooth"} );
+
 });
